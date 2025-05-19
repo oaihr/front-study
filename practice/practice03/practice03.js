14bba0982f548419b5731b3ec11ff3ed7a692c4d
oaihr <rhksgh0710@gmail.com>
2025-05-19 11:30:57 +0900

    js 연습3 조건에 따라 문자열 추가(수정)

--- practice/practice03/practice03.js
+++ practice/practice03/practice03.js
@@ -2,21 +2,35 @@ let strUrl1 = "https://human.or.kr";
 let strUrl2 = "http://human.or.kr";
 let strUrl3 = "http.human.or.kr";
 
+// HTTP (Hyper Text Transfer Protocol)
+// HTTPS (Hyper Text Transfer Protocol over Secure Socket Layer) http에 보안 적용 
+
 /* {코딩부분} */
-function changeLink(str){
-    if(str.includes("://")){
-        if(str.includes("https")){
+function changeLink(str) {
+    if (str.includes("://")) {
+        // substring 이용
+        /* if (str.substring(0, 5) == "https") {
             let insertStr = "secure."
-            let result = str.substring(0, str.indexOf("/")+2) + insertStr + str.substring(str.indexOf("/")+2);
+            // let result = str.substring(0, str.indexOf("/")+2) + insertStr + str.substring(str.indexOf("/")+2);
+            let result = "https://" + insertStr + str.substring(str.indexOf("/") + 2);
             return result;
-        }else if(str.includes("http")){
+        } else if (str.substring(0, 5) == "http:") {
             let insertStr = "open."
-            let result = str.substring(0, str.indexOf("http")+4) + "s"+ 
-                            str.substring(str.indexOf("http")+4, str.indexOf("/")+2) + 
-                            insertStr + str.substring(str.indexOf("/")+2);
+            // let result = str.substring(0, str.indexOf("http")+4) + "s"+ 
+            //                 str.substring(str.indexOf("http")+4, str.indexOf("/")+2) + 
+            //                 insertStr + str.substring(str.indexOf("/")+2);
+            let result = "https://" + insertStr + str.substring(str.indexOf("/") + 2);
             return result;
+        } */
+
+        // split 이용 
+        let splitStr = str.split("://");
+        if (splitStr[0] == 'https') {
+            return splitStr[0] + "://secure." + splitStr[1];
+        } else {
+            return splitStr[0] + "s://open." + splitStr[1];
         }
-    }else{
+    } else {
         return "경로가 잘못되었습니다.";
     }
 }
